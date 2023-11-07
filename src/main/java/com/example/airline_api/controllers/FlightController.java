538b086d358e38a9b14a33f51c08c1d5564083ee
Joseph Vincenzo Carpentieri <joecarpentieri1@gmail.com>
2023-11-07 16:58:35 +0000

    displayed details of specific flights

--- src/main/java/com/example/airline_api/controllers/FlightController.java
+++ src/main/java/com/example/airline_api/controllers/FlightController.java
@@ -1,7 +1,9 @@
 package com.example.airline_api.controllers;
 
 import com.example.airline_api.models.Flight;
+import com.example.airline_api.services.FlightService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -11,22 +13,26 @@ import java.util.List;
 @RequestMapping("/flights")
 public class FlightController {
 
+    @Autowired
+    FlightService flightService;
+
     // Display all available flights
     @GetMapping
     public ResponseEntity<List<Flight>> getAllFlights(){
-        return null;
+        return new ResponseEntity<>(flightService.displayAllFlights(), HttpStatus.OK);
     }
 
     // Display a specific flight
     @GetMapping(value = "/{id}")
-    public ResponseEntity<Flight> getFlightById(){
-        return null;
+    public ResponseEntity<Flight> getFlightById(@PathVariable(value = "id") Long flightId){
+        return new ResponseEntity<>(flightService.displayFlightById(flightId), HttpStatus.OK);
     }
 
     // Add details of a new flight
     @PostMapping
-    public ResponseEntity<Flight> addNewFlight(){
-        return null;
+    public ResponseEntity<Flight> addNewFlight(@RequestBody Flight flight){
+        flightService.addFlightDetails(flight);
+        return new ResponseEntity<>(flight, HttpStatus.CREATED);
     }
 
     // Book passenger on a flight
--- src/main/java/com/example/airline_api/services/FlightService.java
+++ src/main/java/com/example/airline_api/services/FlightService.java
@@ -1,8 +1,12 @@
 package com.example.airline_api.services;
 
+import com.example.airline_api.models.Flight;
 import com.example.airline_api.repositories.FlightRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.List;
 
 @Service
 public class FlightService {
@@ -11,5 +15,18 @@ public class FlightService {
     FlightRepository flightRepository;
 
 //    ADD DETAILS OF A NEW FLIGHT
-    
+    public void addFlightDetails(Flight flight){
+        flightRepository.save(flight);
+    }
+
+//    DISPLAY ALL FLIGHTS
+    public List<Flight> displayAllFlights(){
+        return flightRepository.findAll();
+    }
+
+//    DISPLAY SPECIFIC FLIGHT'S DETAILS
+    public Flight displayFlightById(Long flightId){
+        return flightRepository.findById(flightId).get();
+    }
+
 }
